6d1af51e92d0766fcbb627b39e79eaf9994ddf77
Matic Konečnik <matic.konecnik@student.um.si>
2017-05-24 20:59:03 +0200

    Fixed nav titles and icons.

--- src/mu_path_handler.erl
+++ src/mu_path_handler.erl
@@ -8,32 +8,38 @@
 
 out(Path) -> out(Path, []).
 out(Path, Context1) ->
-  Context = [{pagetitle, <<"Home">>},
-    {navMenu, [{<<"Registracija uporabnika">>, <<"registration">>}, {<<"Urejanje avatarjev">>, <<"edit_avatar">>},{<<"Vprašalniki"/utf8>>, <<"questionnaires">>}, {<<"Admin vprašalniki"/utf8>>,<<"edit_questionnaires">>}]}],
-  DefaultOut = #{ data => Context ++ Context1},
-  lager:error("DefaultOut: ~p",[DefaultOut]),
+  Context = [
+    {navMenu, [{<<"Domov">>, <<"index">>, <<"fa-home">>}, {<<"Urejanje avatarjev">>, <<"edit_avatar">>, <<"fa-user">>},{<<"Vprašalniki"/utf8>>, <<"questionnaires">>, <<"fa-question-circle">>}, {<<"Admin vprašalniki"/utf8>>,<<"edit_questionnaires">>, <<"fa-edit">>}]}],
+
+  %lager:error("DefaultOut: ~p",[DefaultOut]),
   % #pgr{ status = (http koda), headers = [ headerji ], body = <<binarni body>> }
   case Path of
     <<"/">> ->
       #{ status => 302, headers=>#{<<"Location">> => <<"index">>}};
     <<"/index">> ->
+      DefaultOut = #{ data => Context ++ [{pagetitle, <<"Domov">>}] ++ Context1},
       DefaultOut#{ view => mu_view_index };
     <<"/login">> ->
       #{ view => mu_view_login } ;
     <<"/registration">> ->
+      DefaultOut = #{ data => Context ++ [{pagetitle, <<"Registracija">>}] ++ Context1},
       DefaultOut#{ view => mu_view_registration };
     <<"/edit_avatar">> ->
+      DefaultOut = #{ data => Context ++ [{pagetitle, <<"Urejanje avatarjev">>}] ++ Context1},
       DefaultOut#{ view => mu_view_edit_avatar };
     <<"/questionnaire">> ->
+      DefaultOut = #{ data => Context ++ [{pagetitle, <<"Vprasalnik">>}] ++ Context1},
       DefaultOut#{ view => mu_view_questionnaire };
     <<"/questionnaires">> ->
+      DefaultOut = #{ data => Context ++ [{pagetitle, <<"Vprasalniki">>}] ++ Context1},
       DefaultOut#{ view => mu_view_questionnaires };
     <<"/edit_questionnaires">> ->lager:error("Test: ~p",[Context1]),
       {ok, {false, Questionnaires}} = mu_db:get_questionnaires(),
-      #{ view => mu_view_edit_questionnaires, data => Context ++ [{questionnaires, Questionnaires}] ++ Context1 };
+      #{ view => mu_view_edit_questionnaires, data => Context ++ [{pagetitle, <<"Admin vprasalniki">>}, {questionnaires, Questionnaires}] ++ Context1 };
     <<"/edit_questionnaire">> ->
       % {ok, Html} = mu_view_edit_questionnaire:render(Context),
       % #{ status => 200, headers=>#{<<"content-type">> => <<"text/html">>}, body => render_page(mu_view_edit_questionnaire, Context)};
+      DefaultOut = #{ data => Context ++ [{pagetitle, <<"Home">>}] ++ Context1},
       DefaultOut#{ view => mu_view_edit_questionnaire };
     <<"/jsontest">> ->
       % #{ status => 200, headers=>#{<<"content-type">> => <<"application/json">>}, body => Html};
