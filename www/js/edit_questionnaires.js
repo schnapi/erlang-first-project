8bb28305d929563795a9f0ef84beeccf859f6983
Sandi Krivec <sandi.krivec@hotmail.com>
2017-06-08 10:54:33 +0200

    bug fix

--- www/js/edit_questionnaires.js
+++ www/js/edit_questionnaires.js
@@ -1,19 +1,2 @@
 // $(document).ready(function() {
 // })
-function removeQuestionnaire(cur) {
-  var userdata = JSON.stringify({
-    "remove": $(cur).closest("div").find(".id").val(),
-  });
-  $.post("/api/edit_questionnaire", userdata, function(data) {
-    if(data.error)
-      alert(data.error);
-    else {
-      alert("Ustrezno odstranjeni podatki!");
-      $(cur).closest("div").remove();
-    }
-  })
-
-}
-function newQuestionnaire() {
-  location.href = '/edit_questionnaire';
-}
